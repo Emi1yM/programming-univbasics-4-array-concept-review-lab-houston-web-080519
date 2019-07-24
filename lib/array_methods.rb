cb5127b329379d936a914ec6387ee9a3fe40a10d
Emily M <mle.jo.11@gmail.com>
2019-07-24 18:03:45 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,12 +1,11 @@
 def find_element_index(array, value_to_find)
-  
+  p array.find_index(value_to_find)
 end
 
 def find_max_value(array)
-  # Add your solution here
+  p array.find_max_value
 end
 
 def find_min_value(array)
   # Add your solution here
 end
-
